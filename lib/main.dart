f46df5cd36332870197f2e658097f5e069f53fe9
Melat369 <melattesfaye16@gmail.com>
2023-07-30 04:05:10 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+// import 'package:english_words/english_words.dart';
+// import 'package:provider/provider.dart';
+
+void main(){
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  const MyApp({super.Key});
+
+  @override
+  Widget build(BuildContext context){
+    
+  }
+}
+
